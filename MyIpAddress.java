ee22a734442bd09daacdfa8eac706bd334163dac
dipali <dipali.dongare@tcs.com>
2016-03-15 16:10:26 +0530

    new1234

--- MyIpAddress.java
+++ MyIpAddress.java
@@ -10,8 +10,7 @@ public class MyIpAddress {
         try {
             InetAddress ipAddr = InetAddress.getLocalHost();
             System.out.println(ipAddr.getHostAddress());
-            System.getProperty("os.name");
-            
+                        
 
         } catch (UnknownHostException ex) {
             ex.printStackTrace();
